f1544b28ffbd5219c207ad0abe5fda88c59cec73
Rakesh Kumar Nayak <128237968+royalguy15@users.noreply.github.com>
2024-01-20 21:58:35 +0530

    Java Operators

---
+++ arithmetic.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class arithmetic {
+    public static void main(String args[]) {
+        int A = 10;
+        int B = 5;
+
+        System.out.println("add:" + (A + B));
+        System.out.println("sub:" + (A - B));
+        System.out.println("mul:" + (A * B));
+        System.out.println("div:" + (A / B));
+        System.out.println("modulo:" + (A % B));
+    }
+
+}
---
+++ post.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class post {
+    public static void main(String args[]){
+
+        int a = 10;
+        int b = a++;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+    
+}
